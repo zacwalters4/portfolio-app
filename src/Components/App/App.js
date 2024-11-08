e017c18890263f5abc63de6dc7678d8c28a435b4
Zac W <zacwalters4@gmail.com>
2024-11-08 02:17:03 -0700

    Completely revamp site

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import logo from './name-logo.png'
 
 function App() {
   return (
     <main>
-
+      <img className='logo' src={logo}/>
+      <div className='link-container'>
+        <p>Sofware Development</p>
+        <p>Photography</p>
+        <p>Achievements</p>
+        <p>About Me</p>
+        <p>Resume</p>
+      </div>
     </main>
   );
 }
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Header from './Components/Header/Header';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
+    {/* <Header /> */}
     <App />
   </React.StrictMode>
 );
