260c67427310fac77c073d6c3344e9567a8f7d99
Zac W <zacwalters4@gmail.com>
2024-11-12 01:36:17 -0700

    Add social links to about me section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -16,10 +16,18 @@ const About = ({handleMenuClick}) => {
             <h1>About Me</h1>
             <div className='about-container'>
                 <p className='about-text'>
-                Hello, my name is Zac. I recently graduated from the Turing School of Software & Design and was recently employed with NovaEdge Technology as a Front End Software Engineer. I am heavily motivated by self improvement and competition, and driven to push myself and my development skills to the highest level possible. Outside of software development, I spend my free time powerlifting and gaming competitively.
+                Hello, my name is Zac. I recently graduated from the Turing School of Software & Design and was recently employed with NovaEdge Technology as a Front End Software Engineer. I am heavily motivated by self improvement and competition, and driven to push myself and my development skills to the highest level possible. Outside of software development, I spend my free time powerlifting and gaming competitively. 
                 </p>
                 <div className='socials-container'>
-
+                    <a href={'mailto:https://zacwalters4@gmail.com'}>
+                        <img className='socials-badge' src={'https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white'} alt={'Gmail'}></img>
+                    </a>
+                    <a href={'https://www.linkedin.com/in/zac-walters-67951b250/'}>
+                        <img className='socials-badge' src={'https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white'} alt={'LinkedIn'}></img>
+                    </a>
+                    <a href={'https://github.com/zacwalters4'}>
+                        <img className='socials-badge' src={'https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white'} alt={'GitHub'}></img>
+                    </a>
                 </div>
             </div>
             <button className='back-button' id='menu' onMouseEnter={menuTick} onClick={handleMenuClick}>Back</button>
